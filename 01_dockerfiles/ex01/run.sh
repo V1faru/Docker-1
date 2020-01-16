36771136c513e07ad6bbf44a8a5af1fc1fb73619
Andreas Murtonen <amurtone@c2r6p3.hive.fi>
2020-01-16 15:59:38 +0200

    almost ready

--- 01_dockerfiles/ex01/run.sh
+++ 01_dockerfiles/ex01/run.sh
@@ -1,13 +1,21 @@
+#!/bin/sh
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    11                                                 :+:      :+:    :+:    #
+#    run.sh                                             :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: amurtone <amurtone@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/01/14 17:59:44 by amurtone          #+#    #+#              #
-#    Updated: 2020/01/14 17:59:45 by amurtone         ###   ########.fr        #
+#    Created: 2020/01/16 09:49:12 by amurtone          #+#    #+#              #
+#    Updated: 2020/01/16 09:49:12 by amurtone         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-docker volume ls
+echo "Starting build..."
+docker build -t ex01 .
+
+echo "Build finished. Running..."
+docker run --rm -p 9987:9987/udp -p 10011:10011 -p 30033:30033 ex01
+
+echo "Server is running. Connect with local client to $(docker-machine ip Char)."
+echo "When finished, run \`docker stop teamspeak\`"
